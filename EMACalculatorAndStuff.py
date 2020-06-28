d8ccb85ab3bf7419c7e96e61edc571c9261eb57b
studboii <studboii@users.noreply.github.com>
2020-06-28 17:34:49 +0530

    Changed The EMA to Add 6 vs 21 Flags too
    Data set is larger but much better performance improvements.

--- EMACalculatorAndStuff.py
+++ EMACalculatorAndStuff.py
@@ -21,17 +21,17 @@ with open("NotFucked.csv", "r") as CSVDataFile:
 	ComputeData = reset_my_index(ComputeData)
 	EMADataFile = open("DataWithEMA.csv", "w", newline="\n")
 	EMAWriter = csv.writer(EMADataFile)
-	EMAWriter.writerow(["Date","Price","Chg","Av_Sev","Av_OneFrty","SevOverFortyFlag","Av_Six","Av_TwenOne"])
+	EMAWriter.writerow(["Date","Price","Chg","Av_Sev","Av_OneFrty","SevOverFortyFlag","Av_Six","Av_TwenOne","SixOverTwenOneFlag"])
 	# print(ComputeData.Price[0])
-	EMAWriter.writerow([ComputeData.Date[0],ComputeData.Price[0],ComputeData.Chg[0],ComputeData.Price[0],ComputeData.Price[0],False,ComputeData.Price[0],ComputeData.Price[0]]) #First Row
+	EMAWriter.writerow([ComputeData.Date[0],ComputeData.Price[0],ComputeData.Chg[0],ComputeData.Price[0],ComputeData.Price[0],False,ComputeData.Price[0],ComputeData.Price[0], False]) #First Row
 
 	Mean140,Mean7,Old7,Old06,Old21,Old140 = 0,0,0,0,0,0
-	Flag = False
+	FlagSev, FlagSix = False, False
 	for x in range(1,1726):
 		if(x < 6):
 			MeanPrice = ComputeData[:x].mean().Price
 			Mean06,Mean21,Mean140,Mean7 = MeanPrice,MeanPrice,MeanPrice,MeanPrice
-			Flag = False
+			FlagSev,FlagSix = False, False
 			Old6 = MeanPrice
 			
 		elif (x < 21): #EMA For 6 save old 21 day value
@@ -39,6 +39,7 @@ with open("NotFucked.csv", "r") as CSVDataFile:
 			MeanPrice = ComputeData[:x].mean().Price
 			Mean06 = EMA_Calculator(amt,6,Old06)
 			Mean21,Mean140,Mean7 = MeanPrice,MeanPrice,MeanPrice
+			FlagSev,FlagSix = False, False
 			Old06 = Mean06
 			Old21 = MeanPrice
 		elif(x<70): #EMA for 6,21, save old 70 day value
@@ -46,9 +47,11 @@ with open("NotFucked.csv", "r") as CSVDataFile:
 			Mean06 = EMA_Calculator(amt,6,Old06) #6DayEMA
 			Mean21 = EMA_Calculator(amt,21,Old21)#21 day EMA
 			Old06, Old21 = Mean06, Mean21 
+			FlagSix = Mean06 > Mean21
 			MeanPrice = ComputeData[:x].mean().Price #Mean for 70 and 140
 			Old7 = MeanPrice
 			Mean140,Mean7 = MeanPrice,MeanPrice
+			FlagSev = False
 		elif(x<140):#EMA fro 6,21,70, save old value for 140
 			amt = ComputeData.Price[x]
 			Mean06 = EMA_Calculator(amt,6,Old06) #6DayEMA
@@ -57,7 +60,8 @@ with open("NotFucked.csv", "r") as CSVDataFile:
 			Old6, Old21, Old7 = Mean06, Mean21, Mean21
 			MeanPrice = ComputeData[:x].mean().Price #Mean for 140
 			Mean140 = MeanPrice
-			Flag = Mean7>Mean140
+			FlagSix = Mean06 > Mean21
+			FlagSev = Mean7>Mean140
 			Old140 = MeanPrice
 		else: #Calculate everything boiiiii
 			amt = ComputeData.Price[x]
@@ -66,12 +70,13 @@ with open("NotFucked.csv", "r") as CSVDataFile:
 			Mean7  = EMA_Calculator(amt,70,Old7) #70 day EMA
 			Mean140 = EMA_Calculator(amt,140,Old140) #140 day EMA
 			Old6, Old21, Old7,Old140 = Mean06, Mean21, Mean21, Mean140
-			Flag = Mean7 > Mean140
+			FlagSix = Mean06 > Mean21
+			FlagSev = Mean7>Mean140
 
 
 		
 		#We now write and flush the calculated Values.
-		EMAWriter.writerow([ComputeData.Date[x],ComputeData.Price[x],ComputeData.Chg[x],Mean7,Mean140,Flag,Mean06,Mean21]) #Date,Price,Chg,Av_Sev,Av_OneFrty,flag,six,twentyone
+		EMAWriter.writerow([ComputeData.Date[x],ComputeData.Price[x],ComputeData.Chg[x],Mean7,Mean140,FlagSev,Mean06,Mean21,FlagSix]) #Date,Price,Chg,Av_Sev,Av_OneFrty,FlagFor70>140,six,twentyone,FlagFor6>21
 	
 
 
